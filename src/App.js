7abe399c48fb7d6e238cb338690c4c6e4e7a7d55
Kevin Wei <chenghaowei@gmail.com>
2019-09-05 11:38:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Split from './composition/Split';
+import Tooltip from './composition/Tooltip';
+import Dt from './state/TheDate';
+import Counter from './state/Counter';
+import Tabs from './state/Tabs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
     </div>
-  );
+  }
 }
+
+// function App() {
+//   return (
+//     <Counter count={123} />
+//   );
+// }
+
+// // make 2 tooltips here and another inside the App directly
+// const firstTooltip = (
+//   <Tooltip color='hotpink' message='tooltip message'>
+//    ipsum
+//   </Tooltip>
+// )
+// const secondTooltip = (
+//   <Tooltip color='#126BCC' message='another tooltip message'>
+//     officiis
+//   </Tooltip>
+// )
+
+// function App() {
+//   return (
+//     <main className='App'>
+//       <Split className='left' flexBasis={2}>
+//         This is the content for the left `Split`. 
+//         Lorem {firstTooltip} ipsum dolor sit amet consectetur, 
+//         adipisicing elit. Incidunt ex velit suscipit facere officia?
+//         {/* make another tooltip directly inside the App */}
+//         <Tooltip message='one more tooltip message'>
+//           Necessitatibus?
+//         </Tooltip>
+//       </Split>
+//       <Split className='right'>
+//         This is the content for the right `Split`. 
+//         Inventore aliquid cupiditate suscipit repellat. 
+//         Quaerat quis {secondTooltip} officiis quam fuga. 
+//         Aliquid quo possimus id soluta aspernatur.
+//       </Split>
+//     </main>
+//   );
+// }
 
 export default App;
