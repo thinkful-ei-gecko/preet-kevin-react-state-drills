5dbb16a7c16c9cfd63ca1fc0597f2537538762ad
preet-singh <preetsingh13@gmail.com>
2019-09-05 15:30:40 -0500

    first 2 tests complete

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -2,20 +2,7 @@ import React from 'react';
 
 class Accordion extends React.Component {
   static defaultProps = {
-    sections: [
-      {
-        title: 'Section 1',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      },
-      {
-        title: 'Section 2',
-        content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
-      },
-      {
-        title: 'Section 3',
-        content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-      },
-    ],
+    sections: []
   }
   state = {
     currentIndex: null
@@ -29,7 +16,7 @@ class Accordion extends React.Component {
         {
           this.props.sections.map((section, index) => {
             return (
-              <li>
+              <li key={index}>
                 <button onClick={() => this.handleClick(index)}>{section.title}</button>
                 {this.state.currentIndex === index && <p>{section.content}</p>}
               </li>
@@ -42,3 +29,4 @@ class Accordion extends React.Component {
 }
 
 export default Accordion;
+
--- src/state-drills/Accordion.test.js
+++ src/state-drills/Accordion.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Accordion from './Accordion';
 
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+
+
 describe('<Accordion />', () => {
-  test();
+  test('renders empty given no props', () => {
+    const wrapper = shallow(<Accordion />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  test('renders no section as deafult', () => {
+    const wrapper = shallow(<Accordion sections={sections} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  
 })
